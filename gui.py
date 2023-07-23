c506e007780b4eb5086b1a035ccbc463d26ff3d1
= <=>
2023-07-23 20:48:44 +0530

    Implemented Exit & Complete Button

--- gui.py
+++ gui.py
@@ -6,12 +6,16 @@ input_box = PySimpleGUI.Input(tooltip="Enter todo here", key="todo")
 add_button = PySimpleGUI.Button("Add")
 listbox = PySimpleGUI.Listbox(values=functions.get_todo(), key="todos", 
                               enable_events=True, size=[45, 10])
+
 edit_button = PySimpleGUI.Button("Edit")
+complete_button = PySimpleGUI.Button("Complete")
+exit_button = PySimpleGUI.Button("Exit")
 
 window = PySimpleGUI.Window("My To-Do App", 
                             layout=[[label], 
                                     [input_box, add_button], 
-                                    [listbox, edit_button]],
+                                    [listbox, edit_button, complete_button],
+                                    [exit_button]],
                             font=("Helvetica", 20))
 
 while True:
@@ -19,19 +23,31 @@ while True:
     match event:
         case "Add":
             todos = functions.get_todo()
-            new_todo = values["todo"] + "\n" 
+            new_todo = values['todo'] + "\n" 
             todos.append(new_todo)
             functions.write_todo(todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value='')
         
         case "Edit":
             todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
+            new_todo = values['todo'] + "\n"
             todos = functions.get_todo()
             index = todos.index(todo_to_edit)
             todos[index] = new_todo
             functions.write_todo(todos)
             window['todos'].update(values=todos)
+
+        case "Complete":
+            todo_to_remove = values["todos"][0]
+            todos = functions.get_todo()
+            todos.remove(todo_to_remove)
+            functions.write_todo(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")
+       
+        case "Exit":
+           break
        
         case "todos":
             window['todo'].update(value=values['todos'][0])
