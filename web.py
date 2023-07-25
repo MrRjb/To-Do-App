25cc60a17df16d5185ba44ace27985edc8343880
= <=>
2023-07-25 18:00:15 +0530

    Implemented Add Todo feature

--- web.py
+++ web.py
@@ -1,14 +1,20 @@
 import streamlit
 import functions
 
+todos = functions.get_todo()
+
+def add_todo():
+    todo = streamlit.session_state['new_todo'] + "\n"
+    todos.append(todo)
+    functions.write_todo(todos)    
+
 streamlit.title("My To-Do App")
 streamlit.subheader("A Minimalistic Todo App.")
 streamlit.write("This app is designed to boost your productivity!")
 
-todos = functions.get_todo()
-
 for todo in todos:
     streamlit.checkbox(todo)
     
-streamlit.text_input(label="", placeholder="Type in a new todo here ...", help="Press Enter to apply")
+streamlit.text_input(label="", placeholder="Type in a new todo here ...", 
+                     on_change=add_todo, key='new_todo')
 
